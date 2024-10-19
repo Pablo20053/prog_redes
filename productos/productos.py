499981c544ab7ae6392a000f920326605c666cd0
pablor <juan20451@cbtis75.edu.mx>
2024-10-18 22:08:34 -0600

    Productos

---
+++ productos/productos.py
@@ -0,0 +1,15 @@
+'''
+     Nombre: Juan Pablo López Ramírez
+     Descfripción: API de la NBA
+     Fecha: 17/10/2024
+    
+'''
+import requests
+
+url = "https://fakestoreapi.com/products"
+ 
+productos = requests.get(url)
+
+for prod in productos.json():
+    print(prod["title"] ,  "\t" ,prod["price"])
+
---
+++ productos/productos2.py
@@ -0,0 +1,21 @@
+'''
+     Nombre: Juan Pablo López Ramírez
+     Descfripción: API de la NBA
+     Fecha: 17/10/2024
+    
+'''
+import requests
+import time
+
+while True:
+     id = input('Ingresa el ID de produco :')
+     if id.lower() == 'salir' or id.lower() =='s':
+          break
+
+     url = f"https://fakestoreapi.com/products/{id}"
+ 
+     productos = requests.get(url).json()
+     print(productos)
+     time.sleep(2)
+
+
